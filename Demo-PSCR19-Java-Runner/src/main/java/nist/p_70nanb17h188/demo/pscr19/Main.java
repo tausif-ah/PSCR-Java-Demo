d4ed67e87a556db4a5c5f719617f733874ec3aea
Jiachen Chen <jiachen@winlab.rutgers.edu>
2019-05-16 04:59:41 -0400

    Added test applications for TCPConnectionManager

--- Demo-PSCR19-Java-Runner/src/main/java/nist/p_70nanb17h188/demo/pscr19/Main.java
+++ Demo-PSCR19-Java-Runner/src/main/java/nist/p_70nanb17h188/demo/pscr19/Main.java
@@ -1,20 +1,45 @@
 package nist.p_70nanb17h188.demo.pscr19;
 
-import java.util.Arrays;
-import nist.p_70nanb17h188.demo.pscr19.link.LinkLayer;
-import nist.p_70nanb17h188.demo.pscr19.net.NetLayer;
+import android.app.Application;
+import android.os.Handler;
+import java.net.UnknownHostException;
+import nist.p_70nanb17h188.demo.pscr19.logic.link.TCPConnectionClient;
+import nist.p_70nanb17h188.demo.pscr19.logic.link.TCPConnectionServer;
 
 /**
  * Application that runs the demo.
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.printf("Existing names: %s%n", Arrays.toString(Device.getExistingNames()));
-        Device.setName(Device.NAME_PC1);
-        System.out.printf("I am %s%n", Device.getName());
-        LinkLayer.init();
-        NetLayer.init();
-        System.out.println("Initialized!");
+
+    public static final String TAG = "Main";
+
+
+    public static void main(String[] args) throws InterruptedException, UnknownHostException {
+        if (args.length > 0 && args[0].equals("S")) {
+            TCPConnectionServer.startServer();
+        } else {
+            TCPConnectionClient.startClient();
+        }
     }
-    
+
+
+    public static void testLooper(Application application) throws InterruptedException {
+        Handler h = new Handler(application.getApplicationContext().getMainLooper());
+        System.out.printf("[%d] now%n", System.currentTimeMillis());
+        h.postDelayed(() -> {
+            System.out.printf("[%d] after delay 3s%n", System.currentTimeMillis());
+        }, 3000);
+        h.postDelayed(() -> {
+            System.out.printf("[%d] after delay 2s%n", System.currentTimeMillis());
+            h.post(() -> {
+                System.out.printf("[%d] added immediately%n", System.currentTimeMillis());
+            });
+            h.postAt(() -> {
+                System.out.printf("[%d] poste long before%n", System.currentTimeMillis());
+            }, 0);
+        }, 2000);
+
+        Thread.sleep(5000);
+    }
+
 }
---
+++ Demo-PSCR19-Java-Runner/src/main/java/nist/p_70nanb17h188/demo/pscr19/logic/link/TCPConnectionClient.java
@@ -0,0 +1,45 @@
+package nist.p_70nanb17h188.demo.pscr19.logic.link;
+
+import android.app.Application;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import nist.p_70nanb17h188.demo.pscr19.logic.Device;
+import nist.p_70nanb17h188.demo.pscr19.logic.log.Log;
+
+/**
+ *
+ */
+public class TCPConnectionClient {
+
+    public static final String TAG = "TCPConnectionClient";
+
+    public static void startClient() {
+        Application application = new Application();
+        Log.init(1, application);
+
+        Log.d(TAG, "Existing names: %s%n", Arrays.toString(Device.getExistingNames()));
+        Device.setName(Device.NAME_PC1);
+        Log.d(TAG, "I am %s%n", Device.getName());
+
+        InetAddress addr;
+        try {
+            addr = Inet4Address.getByAddress(new byte[]{127, 0, 0, 1});
+        } catch (UnknownHostException ex) {
+            Log.e(TAG, ex, "Error in setting addr!");
+            return;
+        }
+        InetSocketAddress groupOwnerAddress = new InetSocketAddress(addr, 10305);
+
+        LinkLayer.init(application);
+//        NetLayer.init();
+//        System.out.println("Initialized!");
+        try {
+            Thread.sleep(90000);
+        } catch (InterruptedException ex) {
+        }
+    }
+
+}
